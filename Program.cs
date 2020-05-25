fba4fad66e87af0ca11d82a596759e3900963275
Danilka2001 <57344513+Danilka2001@users.noreply.github.com>
2020-05-25 12:18:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailAddress from = new MailAddress("address@gmail.com", "Name");
+            MailAddress to = new MailAddress("address@yandex.ru");
+
+            MailMessage objectMail = new MailMessage(from, to);
+            objectMail.Subject = "Тест";
+            objectMail.Body = "<h2>Письмо-тест работы smtp-клиента</2>";
+            objectMail.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new NetworkCredential("address@gmail.com", "mypassword");
+            smtp.EnableSsl = true;
+            smtp.Send(objectMail);
+            Console.Read();
+        }
+    }
+}
+
